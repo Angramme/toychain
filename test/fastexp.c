d506608809829b9cabf9533333b4de357c06cda8
rodrigue.tvrn <tavernier.rodrigue@gmail.com>
2022-02-28 16:01:59 +0100

    Perf test for modpow + clean now removes .exe

--- test/fastexp.c
+++ test/fastexp.c
@@ -1,7 +1,10 @@
 #include "test/test.h"
 #include "lib/fastexp.h"
+#include "clocktime.h"
 
 int main(){
+
+    //Test de validite
     TEST(modpow_naive(3, 3, 11), 5);
     TEST(modpow_naive(2, 10, 69), 58);
 
@@ -9,4 +12,18 @@ int main(){
     TEST(modpow(2, 10, 69), 58);
 
     TEST_SUMMARY(); 
+    
+
+    //Test de performances
+    /*
+    double dur_naiv;
+    double dur;
+    FILE *f = fopen("plotfastexp1.txt", "w");
+    for(int m = 1; m <=200000000; m*=2){
+        CLOCK_TIME(dur_naiv, modpow_naive(9435000329,m,1093));
+        CLOCK_TIME(dur, modpow(3,m,10));
+        fprintf(f, "%d %f %f\n", m, dur_naiv, dur);
+    }
+    fclose(f);
+    */
 }
