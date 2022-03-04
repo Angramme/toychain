545fd517399198d0184bdff17f7d373af48964a4
Angramme <kacpero376@gmail.com>
2022-03-04 21:44:37 +0100

    added docs to types.h

--- lib/types.h
+++ lib/types.h
@@ -1,9 +1,29 @@
 #ifndef __TYPES_H
 #define __TYPES_H
 
+/**
+ * @file types.h
+ * @brief a file with comfy aliases for long type signatures
+ */
+
+/**
+ * @brief alias for long int
+ */
 #define int32 long int
+
+/**
+ * @brief alias for long long int
+ */
 #define int64 long long int
+
+/**
+ * @brief alias for unsigned long int
+ */
 #define uint32 unsigned long int
+
+/**
+ * @brief alias for unsigned long long int
+ */
 #define uint64 unsigned long long int
 
 #endif
