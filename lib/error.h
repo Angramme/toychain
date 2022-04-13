7bbc5d05323a77f1741eb1e2691b70d8e7f632ee
Rodrigue T <tavernier.rodrigue@gmail.com>
2022-04-13 14:23:42 +0200

    updated error.h

--- lib/error.h
+++ lib/error.h
@@ -1,8 +1,12 @@
 #ifndef _ERROR_H
 #define _ERROR_H
-
 #include <stdio.h>
 
-#define MALLOC_ERROR(msg) fprintf(stderr, "malloc error at %d in file %s \n  message: %s \n", __LINE__, __FILE__, (msg));
+/**
+ * @file error.h
+ * @brief Functions to handle errors message.
+ */
 
+#define MALLOC_ERROR(msg) fprintf(stderr, "malloc error at %d in file %s in function %s\n message : %s \n", __LINE__, __FILE__, __func__, (msg));
+#define FILE_ERROR(msg) fprintf(stderr, "file error at %d in file %s in function %s\n message : %s\n", __LINE__, __FILE__, __func__, (msg));
 #endif
