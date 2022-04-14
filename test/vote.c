180b0b0a03dc4712cc313ac32444632025e497cc
Rodrigue T <tavernier.rodrigue@gmail.com>
2022-04-14 13:54:05 +0200

    fixed compute_winner test

--- test/vote.c
+++ test/vote.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 
 int main(){
     srand(time(NULL));
@@ -45,7 +46,25 @@ int main(){
     TEST_SECTION_END();
 
     TEST_SECTION(compute_winner);
-    {
+    {   
+        //as there is no way to check if the winner is valid, we create an election in test/comput_winner_data,
+        //in which we know who is going to win.
+        CellKey* vots = read_public_keys("./compute_winner_data/keys.txt");
+        CellKey* cands = read_public_keys("./compute_winner_data/candidates.txt");
+        CellProtected* decl = read_protected("./compute_winner_data/declarations.txt");
+        Key* win = compute_winner(decl, cands, vots, 5, 10);
+        char* strw = key_to_str(win);
+        char true_winner[20] = "(ba8f5, 2b537f)";
+        //we know the public key of the winner should be (ba8f5, 2b537f).
+        TEST_MSG(strcmp(strw, true_winner), 0, "compute_winner failed to find winner");
+        free_list_keys(vots);
+        free_list_keys(cands);
+        free_list_protected(decl);
+        free(win);
+        free(strw);
+
+        /*
+        //election simulation (doesn't test is winner is correct)
         int i;
         for(i=0; i<5; i++){
             int sizeV = 100;
@@ -57,13 +76,14 @@ int main(){
             CellProtected* declarations = read_protected("../data/temp/declarations.txt");
             Key* winner = compute_winner(declarations, candidates, voters, sizeC, sizeV);
             char* strwin = key_to_str(winner);
+            printf("winner is : %s\n", strwin);
 
             free_list_keys(voters);
             free_list_keys(candidates);
             free_list_protected(declarations);
             free(winner);
             free(strwin);
-        }
+        }*/
     }
     TEST_SECTION_END();
 
